62bb0e0fd636db45f2c785530bf1ad59fce86290
Jeko <57463439+Jeko-Mus@users.noreply.github.com>
2022-03-10 16:42:32 +0100

    trees exercise updated

---
+++ Week_Chapters/07. Gradient Boosting and Encoding/predict.py
@@ -0,0 +1,16 @@
+
+model = ""
+while True:
+
+    age = int(input("How old are you? \n"))
+    child = int(input("How many children do you have? \n"))
+    smoke = bool(input("Do you smoke? \n"))
+    bmi = float(input("What is your BMI? \n"))
+    region = object(input("Which area do you live? northeast, southeast, southwest or northwest? \n"))
+
+    '''
+    Preprocess
+    predict
+    
+    '''
+    print("You are too fucked up 1 milly")
